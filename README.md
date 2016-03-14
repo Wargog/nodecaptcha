b54e7aaf620a1bc7116cc67d7c14ed42248b1919
Joshua Stroup <wargog@omacostudio.com>
2016-03-14 10:00:27 -0400

    Add JS files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # nodecaptcha
 A very simple Node package for Google's Nocaptcha Recaptcha
+
+# Usage
+```
+recaptcha = require('recaptcha')
+recaptcha.verify(SECRET_KEY, RECAPTCHA_RESPONSE, (success) => {
+	if(success) {
+		do_some_stuff()
+	} else {
+		tell_user_they_goofed()
+	}
+})
+```
---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * Created by JoshuaStroup on 3/13/2016.
+ */
+var https = require('https');
+
+var Recaptcha = {};
+
+Recaptcha.verify = function(key, response, callback) {
+  var opts = {
+    host: 'www.google.com',
+    port: 443,
+    path: '/recaptcha/api/siteverify',
+    method: 'POST',
+    body: JSON.stringify({ secret: key, response: response })
+  };
+
+  var req = https.request(opts, function(res) {
+    var body = [];
+    res.on('data', function(chunk) {
+      body.push(chunk);
+    }).on('end', function() {
+      body = Buffer.concat(body).toString();
+      response = JSON.parse(body);
+
+      if(response.success) {
+        callback.call(this, true);
+      } else {
+        for(var i = 0; i < response['error-codes'].length; i++)
+          console.log(response['error-codes'][i]);
+        callback.call(this, false);
+      }
+    });
+  });
+  req.end();
+
+  req.on('error', function(error) {
+    console.log(error);
+    callback.call(this, false);
+  });
+};
+
+module.exports = Recaptcha;
